9253f536f6e4224cfc1bd53a14ba8bf12a25aa6f
ReginaLordianto <32347032+ReginaLordianto@users.noreply.github.com>
2017-12-15 08:54:04 +0800

    tgs01php

--- 51015015/tgsphp01/mhss.php
+++ 51015015/tgsphp01/mhss.php
@@ -11,12 +11,12 @@ class mhss
   private $warna;
   private $pros;
   private $data =[
-    '51015010'=>['nama'=>'Ivan Darmawan','email'=>'ivandarmawan_@kharisma.ac.id','sex'=>'lk','warna'=>'','pros'=>'002'],
-    '51015011'=>['nama'=>'Jessica Chandra','email'=>'jessicachandra_@kharisma.ac.id','sex'=>'pr','warna'=>'','pros'=>'003'],
-    '51015012'=>['nama'=>'Kenny','email'=>'kenny_@kharisma.ac.id','sex'=>'lk','warna'=>'','pros'=>'001'],
-    '51015013'=>['nama'=>'Mesa','email'=>'mesa_@kharisma.ac.id','sex'=>'pr','warna'=>'','pros'=>'002'],
-    '51015014'=>['nama'=>'Michael Umboh','email'=>'michaelumboh_@kharisma.ac.id','sex'=>'lk','warna'=>'','pros'=>'004'],
-    '51015015'=>['nama'=>'Regina Lordianto','email'=>'reginalordianto_@kharisma.ac.id','sex'=>'pr','warna'=>'','pros'=>'001'],
+    '51015010'=>['nama'=>'Ivan Darmawan','email'=>'ivandarmawan_@kharisma.ac.id','sex'=>'lk','warna'=>'a264df','pros'=>'002'],
+    '51015011'=>['nama'=>'Jessica Chandra','email'=>'jessicachandra_@kharisma.ac.id','sex'=>'pr','warna'=>'768efc','pros'=>'003'],
+    '51015012'=>['nama'=>'Kenny','email'=>'kenny_@kharisma.ac.id','sex'=>'lk','warna'=>'ffeb69','pros'=>'001'],
+    '51015013'=>['nama'=>'Mesa','email'=>'mesa_@kharisma.ac.id','sex'=>'pr','warna'=>'91ee98','pros'=>'002'],
+    '51015014'=>['nama'=>'Michael Umboh','email'=>'michaelumboh_@kharisma.ac.id','sex'=>'lk','warna'=>'e5e186','pros'=>'004'],
+    '51015015'=>['nama'=>'Regina Lordianto','email'=>'reginalordianto_@kharisma.ac.id','sex'=>'pr','warna'=>'fc7677','pros'=>'001']
   ];
 
   function mhss($nim){
